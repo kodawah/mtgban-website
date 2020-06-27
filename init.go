36deaf5b3e4978ba08311e06966433578acd27c5
Vittorio Giovara <vittorio.giovara@gmail.com>
2020-06-27 16:40:05 +0200

    cosmetics: Move generic file functions to a separate file

--- init.go
+++ init.go
@@ -26,41 +26,6 @@ func loadDB() error {
 	return mtgdb.RegisterWithPaths("allprintings.json", "allcards.json")
 }
 
-func fileExists(filename string) bool {
-	fi, err := os.Lstat(filename)
-	if os.IsNotExist(err) {
-		return false
-	}
-	if fi.Mode()&os.ModeSymlink == os.ModeSymlink {
-		link, err := os.Readlink(filename)
-		if err != nil {
-			return false
-		}
-		fi, err = os.Stat(link)
-		if os.IsNotExist(err) {
-			return false
-		}
-		return !fi.IsDir()
-	}
-	return !fi.IsDir()
-}
-
-func fileDate(filename string) time.Time {
-	fi, err := os.Lstat(filename)
-	if os.IsNotExist(err) {
-		return time.Now()
-	}
-	return fi.ModTime()
-}
-
-func mkDirIfNotExisting(dirName string) error {
-	_, err := os.Stat(dirName)
-	if os.IsNotExist(err) {
-		err = os.MkdirAll(dirName, 0700)
-	}
-	return err
-}
-
 func loadInventoryFromFile(info mtgban.ScraperInfo, fname string) (mtgban.Seller, error) {
 	// Get file path from symlink
 	link, err := os.Readlink(fname)
---
+++ utils.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"os"
+	"time"
+)
+
+func fileExists(filename string) bool {
+	fi, err := os.Lstat(filename)
+	if os.IsNotExist(err) {
+		return false
+	}
+	if fi.Mode()&os.ModeSymlink == os.ModeSymlink {
+		link, err := os.Readlink(filename)
+		if err != nil {
+			return false
+		}
+		fi, err = os.Stat(link)
+		if os.IsNotExist(err) {
+			return false
+		}
+		return !fi.IsDir()
+	}
+	return !fi.IsDir()
+}
+
+func fileDate(filename string) time.Time {
+	fi, err := os.Lstat(filename)
+	if os.IsNotExist(err) {
+		return time.Now()
+	}
+	return fi.ModTime()
+}
+
+func mkDirIfNotExisting(dirName string) error {
+	_, err := os.Stat(dirName)
+	if os.IsNotExist(err) {
+		err = os.MkdirAll(dirName, 0700)
+	}
+	return err
+}
