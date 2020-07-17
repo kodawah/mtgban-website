a11bf4871d96f1daebfdb27e441cbd74ab316eb7
Vittorio Giovara <vittorio.giovara@gmail.com>
2020-07-18 01:24:58 +0200

    search: Add comments throughout the search loop

--- search.go
+++ search.go
@@ -38,15 +38,19 @@ func Search(w http.ResponseWriter, r *http.Request) {
 
 	query := r.FormValue("q")
 
+	// Query is not null, let's get processing
 	if query != "" {
 		log.Println(query)
 
+		// Keep track of what was searched
 		pageVars.SearchQuery = query
+		// Setup conditions keys, all etnries, and images
 		pageVars.CondKeys = []string{"TCG", "NM", "SP", "MP", "HP", "PO"}
 		pageVars.FoundSellers = map[mtgdb.Card]map[string][]mtgban.CombineEntry{}
 		pageVars.FoundVendors = map[mtgdb.Card][]mtgban.CombineEntry{}
 		pageVars.Images = map[mtgdb.Card]string{}
 
+		// Filter out any element from the search syntax
 		filterEdition := ""
 		filterCondition := ""
 		filterFoil := ""
@@ -81,6 +85,7 @@ func Search(w http.ResponseWriter, r *http.Request) {
 			}
 		}
 
+		// Set which comparison function to use depending on the search syntax
 		cmpFunc := mtgjson.NormPrefix
 		if strings.HasPrefix(query, "\"") && strings.HasSuffix(query, "\"") {
 			cmpFunc = mtgjson.NormEquals
@@ -94,27 +99,35 @@ func Search(w http.ResponseWriter, r *http.Request) {
 			query = strings.TrimSpace(query)
 		}
 
+		// Handle split cards
 		if strings.Contains(query, " // ") {
 			s := strings.Split(query, " // ")
 			query = s[0]
 		}
 
+		// Search sellers
 		for i, seller := range Sellers {
 			if seller == nil {
 				log.Println("nil seller at position", i)
 				continue
 			}
 
+			// Get inventory
 			inventory, err := seller.Inventory()
 			if err != nil {
 				log.Println(err)
 				continue
 			}
+
+			// Loop through cards
 			for card, entries := range inventory {
+				// Run the comparison function set above
 				if cmpFunc(card.Name, query) {
+					// Skip cards that are not of the desired set
 					if filterEdition != "" && filterEdition != card.Edition {
 						continue
 					}
+					// Skip cards that are not as desired foil
 					if filterFoil != "" {
 						foilStatus, err := strconv.ParseBool(filterFoil)
 						if err == nil {
@@ -126,34 +139,44 @@ func Search(w http.ResponseWriter, r *http.Request) {
 						}
 					}
 
+					// Load up image links
 					if pageVars.Images[card] == "" {
 						code, _ := mtgdb.EditionName2Code(card.Edition)
 						link := fmt.Sprintf("https://api.scryfall.com/cards/%s/%s?format=image&version=normal", strings.ToLower(code), card.Number)
 						pageVars.Images[card] = link
 					}
 
+					// Loop thorugh available conditions
 					for _, entry := range entries {
+						// Skip cards that have not the desired condition
 						if filterCondition != "" && filterCondition != entry.Conditions {
 							continue
 						}
+
+						// No price no dice
 						if entry.Price == 0 {
 							continue
 						}
 
+						// Check if card already has any entry
 						_, found := pageVars.FoundSellers[card]
 						if !found {
 							pageVars.FoundSellers[card] = map[string][]mtgban.CombineEntry{}
 						}
 
+						// Set conditions - handle the special TCG one that appears
+						// at the top of the results
 						conditions := entry.Conditions
 						if seller.Info().Name == "TCG Low" || seller.Info().Name == "TCG Direct Low" {
 							conditions = "TCG"
 						}
+						// Check if the current entry has any condition
 						_, found = pageVars.FoundSellers[card][conditions]
 						if !found {
 							pageVars.FoundSellers[card][conditions] = []mtgban.CombineEntry{}
 						}
 
+						// Prepare all the deets
 						res := mtgban.CombineEntry{
 							ScraperName: seller.Info().Name,
 							Price:       entry.Price,
@@ -163,12 +186,15 @@ func Search(w http.ResponseWriter, r *http.Request) {
 						if seller.Info().CountryFlag != "" {
 							res.ScraperName += " " + seller.Info().CountryFlag
 						}
+
+						// Touchdown
 						pageVars.FoundSellers[card][conditions] = append(pageVars.FoundSellers[card][conditions], res)
 					}
 				}
 			}
 		}
 
+		// Really same as above
 		for i, vendor := range Vendors {
 			if vendor == nil {
 				log.Println("nil vendor at position", i)
