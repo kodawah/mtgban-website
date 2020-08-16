9072bc3a838c61ec203a73dafa6d5615cbf43a6e
Vittorio Giovara <vittorio.giovara@gmail.com>
2020-08-16 14:11:26 +0200

    Update mtgmatcher too when updating mtgban, skip vendoring

--- update-mtgban.sh
+++ update-mtgban.sh
@@ -3,5 +3,9 @@
 TIP=$(git ls-remote https://github.com/kodabb/go-mtgban.git HEAD | awk '{ print $1}')
 
 GOSUMDB=off go get -u github.com/kodabb/go-mtgban@$TIP
+
+TIP=$(git ls-remote https://github.com/kodabb/go-mtgmatcher.git HEAD | awk '{ print $1}')
+
+GOSUMDB=off go get -u github.com/kodabb/go-mtgmatcher@$TIP
+
 go mod tidy
-go mod vendor
