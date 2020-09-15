2fd1fa089a6c8dc14b676f1ff922a71408f3a80a
Vittorio Giovara <vittorio.giovara@gmail.com>
2020-09-15 10:44:05 +0200

    Do not download mtgjson v4 files any more

--- get-mtgjson.sh
+++ get-mtgjson.sh
@@ -2,11 +2,5 @@
 
 set -e
 
-curl "https://www.mtgjson.com/files/AllCards.json" > /tmp/allcards.json
-mv /tmp/allcards.json .
-
-curl "https://www.mtgjson.com/files/AllPrintings.json" > /tmp/allprintings.json
-mv /tmp/allprintings.json .
-
 curl "https://mtgjson.com/api/v5/AllPrintings.json" > /tmp/allprintings5.json
 mv /tmp/allprintings5.json .
