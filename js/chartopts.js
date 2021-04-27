aaa11ebb4ef037e1decbffe4e4c10bd711e74e74
Vittorio Giovara <vittorio.giovara@gmail.com>
2021-04-27 03:51:26 +0200

    search/chart: Draw a vertical line that follows focus

--- js/chartopts.js
+++ js/chartopts.js
@@ -1,3 +1,30 @@
+// Custom chart type which copies the "line" one and draws
+// an extra vertical line on hover
+Chart.defaults.banWithLine = Chart.defaults.line;
+Chart.controllers.banWithLine = Chart.controllers.line.extend({
+    draw: function(ease) {
+        Chart.controllers.line.prototype.draw.call(this, ease);
+
+        if (this.chart.tooltip._active && this.chart.tooltip._active.length) {
+            var activePoint = this.chart.tooltip._active[0];
+            var ctx = this.chart.ctx;
+            var x = activePoint.tooltipPosition().x;
+            var topY = this.chart.legend.bottom;
+            var bottomY = this.chart.chartArea.bottom;
+
+            // Draw vertical line
+            ctx.save();
+            ctx.beginPath();
+            ctx.moveTo(x, topY);
+            ctx.lineTo(x, bottomY);
+            ctx.lineWidth = 2;
+            ctx.strokeStyle = '#92929242';
+            ctx.stroke();
+            ctx.restore();
+        }
+    }
+});
+
 // Custom positioner to draw the tooltip on the bottom
 Chart.Tooltip.positioners.bottom = function(elements, position) {
     if (!elements.length) {
@@ -26,6 +53,7 @@ function getChartOpts() {
         tooltips: {
             mode: "index",
             position: "bottom",
+            backgroundColor: 'rgba(10, 10, 10, 220)',
             intersect: false,
             callbacks: {
                 // Make sure there is a $ and floating point looks sane
@@ -38,6 +66,7 @@ function getChartOpts() {
         hover: {
             mode: "x-axis",
             intersect: true,
+            animationDuration: 0,
         },
         scales: {
             xAxes: [
