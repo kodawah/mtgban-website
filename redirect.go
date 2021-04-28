a13e64e66111be07c572764b633640c4cfa744c9
Vittorio Giovara <vittorio.giovara@gmail.com>
2021-04-28 18:49:52 +0200

    discord: Set a custom utm paramter for TCG links

--- redirect.go
+++ redirect.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"net/http"
+	"net/url"
 	"strings"
 )
+
+const UTM_BOT = "banbot"
 
 func Redirect(w http.ResponseWriter, r *http.Request) {
 	path := strings.TrimPrefix(r.URL.Path, "/go/")
@@ -24,7 +27,19 @@ func Redirect(w http.ResponseWriter, r *http.Request) {
 					}
 					entries := inv[hash]
 					for _, entry := range entries {
-						http.Redirect(w, r, entry.URL, http.StatusFound)
+						link := entry.URL
+						// Change the utm default query param to improve tracking
+						if strings.HasPrefix(store, "TCG") {
+							u, err := url.Parse(link)
+							if err != nil {
+								break
+							}
+							v := u.Query()
+							v.Set("utm_medium", UTM_BOT)
+							u.RawQuery = v.Encode()
+							link = u.String()
+						}
+						http.Redirect(w, r, link, http.StatusFound)
 						return
 					}
 				}
