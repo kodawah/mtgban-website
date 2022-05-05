d2165e2d192c901b410ddcd364404a9584511bdf
Vittorio Giovara <vittorio.giovara@gmail.com>
2022-05-05 16:22:46 -0400

    Delete cookie if no data is set

--- js/cookies.js
+++ js/cookies.js
@@ -1,6 +1,12 @@
 function setCookie(cname, cvalue, exdays) {
     const d = new Date();
     d.setTime(d.getTime() + (exdays*24*60*60*1000));
+
+    // Delete cookie if no data
+    if (cvalue == '') {
+        d.setTime(Date.now());
+    }
+    
     let expires = "expires="+ d.toUTCString();
     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/;SameSite=Strict";
 }
