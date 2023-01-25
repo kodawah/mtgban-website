667196945ea519e35ac552af8d738e05a0fdfa47
Vittorio Giovara <vittorio.giovara@gmail.com>
2023-01-24 19:48:20 -0500

    upload: Support 'expansion' as edition header name

--- upload.go
+++ upload.go
@@ -652,12 +652,12 @@ func parseHeader(first []string) (map[string]int, error) {
 			if !found {
 				indexMap["id"] = i
 			}
-		case (strings.Contains(field, "name") && !strings.Contains(field, "edition") && !strings.Contains(field, "set")) || field == "card":
+		case (strings.Contains(field, "name") && !strings.Contains(field, "edition") && !strings.Contains(field, "set") || strings.Contains(field, "expansion")) || field == "card":
 			_, found := indexMap["cardName"]
 			if !found {
 				indexMap["cardName"] = i
 			}
-		case strings.Contains(field, "edition") || strings.Contains(field, "set"):
+		case strings.Contains(field, "edition") || strings.Contains(field, "set") || strings.Contains(field, "expansion"):
 			_, found := indexMap["edition"]
 			if !found {
 				indexMap["edition"] = i
