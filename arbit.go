ac3cefea68db9fd3caa2a984f0fdc4b9d8057c8e
Vittorio Giovara <vittorio.giovara@gmail.com>
2023-03-02 01:56:45 +0100

    Avoid using a shared array for scraper names

--- arbit.go
+++ arbit.go
@@ -286,9 +286,6 @@ func arbit(w http.ResponseWriter, r *http.Request, reverse bool) {
 			}
 			vendorKeys = append(vendorKeys, vendor.Info().Shorthand)
 		}
-		sort.Slice(vendorKeys, func(i, j int) bool {
-			return ScraperNames[vendorKeys[i]] < ScraperNames[vendorKeys[j]]
-		})
 		pageVars.VendorKeys = vendorKeys
 	} else {
 		filters := strings.Split(readCookie(r, "ArbitVendorsList"), ",")
