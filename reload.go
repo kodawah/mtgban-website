9e6defafe0646bfe9ac9bb07a240e575d0e59187
Vittorio Giovara <vittorio.giovara@gmail.com>
2023-03-02 02:15:14 +0100

    Rework crons to avoid reloading scrapers, refresh infos every 12h

--- reload.go
+++ reload.go
@@ -2,117 +2,12 @@ package main
 
 import (
 	"errors"
-	"fmt"
 	"path"
 	"time"
 
 	"github.com/mtgban/go-mtgban/mtgban"
 	"github.com/mtgban/go-mtgban/tcgplayer"
-	"golang.org/x/exp/slices"
 )
-
-func reloadCK() {
-	reloadSingle("cardkingdom")
-}
-
-func reloadSCG() {
-	reloadSingle("starcitygames")
-}
-
-func reloadSingle(name string) {
-	defer recoverPanicScraper()
-
-	ServerNotify("refresh", "Reloading "+name)
-
-	// Lock because we plan to load both sides of the scraper
-	ScraperOptions[name].Mutex.Lock()
-	ScraperOptions[name].Busy = true
-	defer func() {
-		ScraperOptions[name].Busy = false
-		ScraperOptions[name].Mutex.Unlock()
-	}()
-
-	scraper, err := ScraperOptions[name].Init(ScraperOptions[name].Logger)
-	if err != nil {
-		msg := fmt.Sprintf("error initializing %s: %s", name, err.Error())
-		ServerNotify("refresh", msg, true)
-		return
-	}
-
-	// These functions will update the global scraper only if it was
-	// previously added to the slice of Sellers or Vendors via the
-	// client Register functions
-	updateSellers(scraper)
-	updateVendors(scraper)
-
-	ServerNotify("refresh", name+" refresh completed")
-}
-
-func reloadTCG() {
-	reloadMarket("tcg_index")
-	reloadMarket("tcg_market")
-
-	loadTCGDirectNet(Vendors)
-
-	ServerNotify("refresh", "tcg fully refreshed")
-}
-
-func reloadMarket(name string) {
-	defer recoverPanicScraper()
-
-	ServerNotify("refresh", "Reloading "+name)
-
-	// Lock because we plan to load both sides of the scraper
-	ScraperOptions[name].Mutex.Lock()
-	ScraperOptions[name].Busy = true
-	defer func() {
-		ScraperOptions[name].Busy = false
-		ScraperOptions[name].Mutex.Unlock()
-	}()
-
-	scraper, err := ScraperOptions[name].Init(ScraperOptions[name].Logger)
-	if err != nil {
-		msg := fmt.Sprintf("error initializing %s: %s", name, err.Error())
-		ServerNotify("refresh", msg, true)
-		return
-	}
-
-	multiSellers, err := mtgban.Seller2Sellers(scraper.(mtgban.Market))
-	if err != nil {
-		msg := fmt.Sprintf("error separating %s: %s", name, err.Error())
-		ServerNotify("refresh", msg)
-		return
-	}
-
-	keepers := ScraperOptions[name].Keepers
-	for i := range multiSellers {
-		// Skip subsellers not explicitly enabled
-		if !slices.Contains(keepers, multiSellers[i].Info().Shorthand) {
-			continue
-		}
-		updateSellers(multiSellers[i])
-	}
-
-	// This can be done because only the already-registered scrapers
-	// will be updated, no effect otherwise
-	updateVendors(scraper)
-
-	ServerNotify("refresh", name+" market refresh completed")
-}
-
-func updateSellers(scraper mtgban.Scraper) {
-	for i := range Sellers {
-		if Sellers[i] != nil && Sellers[i].Info().Shorthand == scraper.Info().Shorthand {
-			err := updateSellerAtPosition(scraper.(mtgban.Seller), i, false)
-			if err != nil {
-				msg := fmt.Sprintf("seller %s %s - %s", scraper.Info().Name, scraper.Info().Shorthand, err.Error())
-				ServerNotify("refresh", msg, true)
-				continue
-			}
-			ServerNotify("refresh", scraper.Info().Shorthand+" inventory updated")
-		}
-	}
-}
 
 func updateSellerAtPosition(seller mtgban.Seller, i int, andLock bool) error {
 	opts := ScraperOptions[ScraperMap[seller.Info().Shorthand]]
@@ -142,20 +37,6 @@ func updateSellerAtPosition(seller mtgban.Seller, i int, andLock bool) error {
 	targetDir := path.Join(InventoryDir, time.Now().Format("2006-01-02/15"))
 	go uploadSeller(Sellers[i], targetDir)
 	return nil
-}
-
-func updateVendors(scraper mtgban.Scraper) {
-	for i := range Vendors {
-		if Vendors[i] != nil && Vendors[i].Info().Shorthand == scraper.Info().Shorthand {
-			err := updateVendorAtPosition(scraper.(mtgban.Vendor), i, false)
-			if err != nil {
-				msg := fmt.Sprintf("vendor %s %s - %s", scraper.Info().Name, scraper.Info().Shorthand, err.Error())
-				ServerNotify("refresh", msg, true)
-				continue
-			}
-			ServerNotify("refresh", scraper.Info().Shorthand+" buylist updated")
-		}
-	}
 }
 
 func updateVendorAtPosition(vendor mtgban.Vendor, i int, andLock bool) error {
